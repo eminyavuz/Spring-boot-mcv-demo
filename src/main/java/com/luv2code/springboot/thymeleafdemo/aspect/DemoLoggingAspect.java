4d7987a7105235704614551d60dfc15d08674bd7
Mahmut Emin Yavuz <91795112+itsaymey@users.noreply.github.com>
2024-11-20 18:54:49 +0300

    project created

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
@@ -0,0 +1,68 @@
+package com.luv2code.springboot.thymeleafdemo.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.logging.Logger;
+
+@Aspect
+@Component
+public class DemoLoggingAspect {
+    // setup logger
+    private Logger  myLogger= Logger.getLogger(getClass().getName());
+
+    // setup pointcut declerations
+
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.controller.*.*(..))")
+    private void forControllerPackage(){
+
+    }
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.service.*.*(..))")
+    private void forServicePackage(){
+
+    }
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.dao.*.*(..))")
+    private void forDaoPackage(){
+
+    }
+    @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage() ")
+    private  void forAppFlow(){
+
+    }
+    @Before("forAppFlow()")
+    private  void before(JoinPoint theJoinPoint){
+        // display method we are calling
+ String theMethod= theJoinPoint.getSignature().toLongString();
+ myLogger.info("======> in @Before : calling method: " + theMethod);
+
+        // display the arguments to the method
+ // get the arguments
+        Object [] args= theJoinPoint.getArgs();
+        // loop thru and display args
+        for(Object tmpArg : args){
+            myLogger.info("====> argument: "+ tmpArg);
+        }
+
+    }
+    // add after returning advice
+    @AfterReturning(
+            pointcut = "forAppFlow()",
+            returning = "theResult"
+    )
+    public void afterReturning(JoinPoint theJoinPoint, Object theResult){
+// display method we are returining from
+  String theMethod= theJoinPoint.getSignature().toShortString();
+  myLogger.info("=====> in @AfterReturning from method : "+ theMethod);
+
+        //  display data returned
+        myLogger.info("=====> result: " + theResult);
+
+    }
+
+
+}
